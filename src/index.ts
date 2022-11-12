3d43f4f168d1fbf37a36dd26cc9af9656c04fc49
Kasper Mikiewicz <IderedPL@gmail.com>
2022-11-12 03:01:38 +0100

    feat: add support for native and function components

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
 import React from "react";
 
-export function twix<Props extends { className?: string }, Element>(
-  component: React.ForwardRefExoticComponent<
-    Props & React.RefAttributes<Element>
-  >,
-  className?: string
-) {
+export function twix<
+  T extends
+    | keyof JSX.IntrinsicElements
+    | React.FunctionComponent
+    | React.ForwardRefExoticComponent<Props>,
+  Element = T extends keyof React.ReactHTML
+    ? React.ReactHTML[T] extends React.DetailedHTMLFactory<any, infer U>
+      ? U
+      : never
+    : T extends React.ForwardRefExoticComponent<
+        unknown & React.RefAttributes<infer U>
+      >
+    ? U
+    : T extends React.FunctionComponent
+    ? ReturnType<T>
+    : never,
+  Props extends { className?: string } = T extends keyof React.ReactHTML
+    ? React.ReactHTML[T] extends React.DetailedHTMLFactory<infer U, any>
+      ? U
+      : never
+    : T extends React.ForwardRefExoticComponent<infer U>
+    ? U
+    : T extends React.FunctionComponent<infer U>
+    ? U
+    : never
+>(component: T, className: string) {
   return React.forwardRef<Element, Props>((props, ref) => {
     return React.createElement(component, {
       ...props,
